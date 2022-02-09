159e214e83332d8822f907aea40e99f9d28ed890
RustamBeast <rusyaf0608@mail.ru>
2022-02-09 20:03:28 +0100

    Что-то

--- client/src/pages/KcalPage.js
+++ client/src/pages/KcalPage.js
@@ -1,12 +1,21 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import Layout from '../components/Layout';
 
 function KcalPage() {
-    return (
-        <Layout>
-            <div>Kcal</div>
-        </Layout>
-    );
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    document.title = `Вы нажали ${count} раз`;
+  }, [count]);
+
+  return (
+    <div>
+      <p>Вы нажали {count} раз</p>
+      <button onClick={() => setCount(count + 1)}>
+        Нажми на меня
+      </button>
+    </div>
+  );
 }
 
 export default KcalPage;
